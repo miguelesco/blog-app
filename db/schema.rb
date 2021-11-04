ff487596c92af95f6ad082f57404a295b5ea6806
miguelesco <miuelangel1999@gmail.com>
2021-11-04 19:18:04 -0400

    create posts migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_221401) do
+ActiveRecord::Schema.define(version: 2021_11_04_231016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2021_11_04_221401) do
     t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
@@ -35,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_11_04_221401) do
   end
 
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
