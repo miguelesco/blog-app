a0c7fc62ccf46bcb14a18fd76bf624e1865dc42d
miguelesco <miuelangel1999@gmail.com>
2021-11-05 11:12:59 -0400

    add missing linters

--- db/migrate/20211104231909_create_likes.rb
+++ db/migrate/20211104231909_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
       t.references :author,  null: false, foreign_key: { to_table: :users }
-      t.references :post_id,  null: false, foreign_key: { to_table: :posts }
+      t.references :post,  null: false, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
