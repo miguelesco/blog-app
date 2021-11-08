97c32f26e9403cf02981a618e19f92d5ff0b72d0
miguelesco <miuelangel1999@gmail.com>
2021-11-08 12:35:00 -0400

    comment and like methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < AplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_post_likes
+    likes_counter = post.likes.count
+    post.update(likes_counter: likes_counter)
+  end
 end
