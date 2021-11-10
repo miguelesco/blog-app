1bb359fb282bd474c82ce4dd91b1862e74848f73
miguelesco <miuelangel1999@gmail.com>
2021-11-10 18:04:41 -0400

    create and new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,23 @@ class PostsController < ApplicationController
     @users = User.all
     @post = @user.posts.find(params[:id])
   end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    new_post = Post.create(title: params[:post][:title], text: params[:post][:text], author_id: 1)
+    respond_to do |format| 
+      format.html do 
+        if new_post.save
+          flash[:notice] = "Saved successfully"
+          redirect_to "/users/#{new_post.author_id}/posts/#{new_post.id}"
+        else
+          flash[:error] = "error"
+          render :new
+        end
+      end
+    end
+  end
 end
