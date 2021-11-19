6dabe80beb4ec3592f55cfaf5ab9972cbad3dda2
jmontanholi <jonunes9326@hotmail.com.br>
2021-11-19 16:22:06 -0300

    Finish api-docs ui with swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
-    'v1/swagger.json' => {
+    'v1/swagger.yaml' => {
       openapi: '3.0.1',
       info: {
         title: 'API V1',
@@ -33,10 +33,10 @@ RSpec.configure do |config|
       },
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: '127.0.0.1:3000'
             }
           }
         }
